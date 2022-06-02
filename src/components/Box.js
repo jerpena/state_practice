9ef0402eefb437acf42686906d536e63742116db
Jeremy Pena <5196790+jerpena@users.noreply.github.com>
2022-06-01 21:40:26 -0500

    add box component

---
+++ src/components/Box.js
@@ -0,0 +1,14 @@
+function Box(props) {
+    const styles = {
+        backgroundColor: "transparent"
+    };
+
+    return (
+        <div
+            style={styles}
+            className="box"
+        ></div>
+    );
+}
+
+export default Box;
